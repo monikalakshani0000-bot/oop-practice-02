a8b9bebbd19b1674b7d91b69d230408b928c09f4
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-17 05:57:48 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+
+		Scanner input = new Scanner(System.in);
+
+		int t =0 ;
+		for(int i=0; i<50; i++){					
+			System.out.print("Input Number : ");
+			int num = input.nextInt();
+			
+			if(num>100){
+				t++;
+			}								
+		}
+		System.out.print("\nNumbers greater than 100: "+t);
+		
+	}
+}
