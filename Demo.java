9ed31ff61de2d1ba3a1743bf6555972aa49b4eb7
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-22 10:16:16 +0530

    add layout button

--- Demo.java
+++ Demo.java
@@ -3,38 +3,30 @@ import java.awt.*;
 class Demo{
 	public static void main(String[] args) {
 		JFrame f1=new JFrame();
-		f1.setSize(1000,1000);
+		f1.setSize(300,300);
 		f1.setTitle("Calculator");
 		f1.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		f1.setLayout(new BorderLayout());
+		JButton btnSouth=new JButton("South");
+		btnSouth.setFont(new Font("",Font.BOLD,25));
+		f1.add("South",btnSouth);
 		
 		JButton btnNorth=new JButton("North");
 		btnNorth.setFont(new Font("",Font.BOLD,25));
-		f1.add(btnNorth, BorderLayout.NORTH);
-		f1.add(btnNorth);
-		
-		JButton btnSouth=new JButton("South");
-		btnSouth.setFont(new Font("",Font.BOLD,25));
-		f1.add(btnNorth, BorderLayout.SOUTH);
-		f1.add(btnSouth);
-		
-		JButton btnEast=new JButton("East");
-		btnEast.setFont(new Font("",Font.BOLD,25));
-		f1.add(btnNorth, BorderLayout.EAST);
-		f1.add(btnEast);
+		f1.add("North",btnNorth);
 		
 		JButton btnWest=new JButton("West");
 		btnWest.setFont(new Font("",Font.BOLD,25));
-		f1.add(btnNorth, BorderLayout.WEST);
-		f1.add(btnWest);
+		f1.add("West",btnWest);
 		
-		/*JButton btnCenter=new JButton("CENTER");
+		JButton btnEast=new JButton("East");
+		btnEast.setFont(new Font("",Font.BOLD,25));
+		f1.add("East",btnEast);
+		
+		JButton btnCenter=new JButton("Center");
 		btnCenter.setFont(new Font("",Font.BOLD,25));
-		f1.add(btnNorth, BorderLayout.NORTH);
-		f1.add(btnCenter);*/
-		
+		f1.add("Center",btnCenter);
 		f1.setVisible(true);
 	}
 }
