073d30f6090effe65aed1b31aaa43efe4d78c51a
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-22 13:38:20 +0530

    cal gui

--- Demo.java
+++ Demo.java
@@ -8,40 +8,22 @@ class Demo{
 		f1.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		JLabel lblTitle=new JLabel("Add Student Form");
-		lblTitle.setFont(new Font("",1,28));
-		lblTitle.setHorizontalAlignment(JLabel.CENTER);
-		f1.add("North",lblTitle);
+		JTextField txtDisplay=new JTextField();
+		txtDisplay.setFont(new Font("",Font.BOLD,25));
+		txtDisplay.setHorizontalAlignment(JTextField.RIGHT);
+		//txtDisplay.setEditable(false);
+		f1.add("North",txtDisplay);
 		
-		JTextField txtId=new JTextField(5);
-		txtId.setFont(new Font("",1,20));
-		JTextField txtName=new JTextField(10);
-		txtName.setFont(new Font("",1,20));
-		JTextField txtPrfMarks=new JTextField(3);
-		txtPrfMarks.setFont(new Font("",1,20));
-		JTextField txtDbmsMarks=new JTextField(3);
-		txtDbmsMarks.setFont(new Font("",1,20));
 		
-		JLabel lblId=new JLabel("Student ID");	
-		lblId.setFont(new Font("",1,20));
-		JLabel lblName=new JLabel("Name");	
-		lblName.setFont(new Font("",1,20));
-		JLabel lblPrfMarks=new JLabel("PRF Marks");	
-		lblPrfMarks.setFont(new Font("",1,20));
-		JLabel lblDbmsMarks=new JLabel("DBMS Marks");	
-		lblDbmsMarks.setFont(new Font("",1,20));
-		
-		JPanel panel=new JPanel(new GridLayout(4,2));	
-		panel.add(lblId);
-		panel.add(txtId);
-		panel.add(lblName);	
-		panel.add(txtName);	
-		panel.add(lblPrfMarks);	
-		panel.add(txtPrfMarks);	
-		panel.add(lblDbmsMarks);
-		panel.add(txtDbmsMarks);
-		f1.add("West",panel);
-		f1.pack();
+		JButton[] buttonArray=new JButton[16];
+		String[] buttonName={"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
+		JPanel buttonPanel=new JPanel(new GridLayout(4,4,4,4));
+		for (int i = 0; i < 16; i++){
+			buttonArray[i]=new JButton(buttonName[i]);
+			buttonArray[i].setFont(new Font("",Font.BOLD,25));
+			buttonPanel.add(buttonArray[i]);
+		}
+		f1.add("Center",buttonPanel);
 		f1.setVisible(true);
 	}
 }
