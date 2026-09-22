dfb59d2aa95d1a66078c57d1e3172d0186a7d92f
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-22 10:59:33 +0530

    button for loop

--- Demo.java
+++ Demo.java
@@ -10,71 +10,26 @@ class Demo{
 		f1.setLayout(new GridLayout(4,4,2,2));
 		
 		
-		JButton btn7=new JButton("7");
-		btn7.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn7);
+		JButton[] buttonArray = new JButton[16];
+		String[] buttonName = {"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
 		
-		JButton btn8=new JButton("8");
-		btn8.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn8);
+		for(int i = 0;i<16;i++){
+			buttonArray[i] = new JButton(buttonName[i]);
+			buttonArray[i].setFont(new Font("",Font.BOLD,25));
+			f1.add(buttonArray[i]);
+		}
+	
+	
 		
-		JButton btn9=new JButton("9");
-		btn9.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn9);
+		/*JButton[] buttonArray=new JButton[16];
+		String[] buttonName={"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
 		
-		JButton btn11=new JButton("*");
-		btn11.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn11);
-		
-		JButton btn4=new JButton("4");
-		btn4.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn4);
-		
-		JButton btn5=new JButton("5");
-		btn5.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn5);
-		
-		JButton btn6=new JButton("6");
-		btn6.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn6);
-		
-		JButton btn12=new JButton("/");
-		btn12.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn12);
-		
-		JButton btn1=new JButton("1");
-		btn1.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn1);
-		
-		JButton btn2=new JButton("2");
-		btn2.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn2);
-		
-		JButton btn3=new JButton("3");
-		btn3.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn3);
-		
-		JButton btn13=new JButton("+");
-		btn13.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn13);
-		
-		JButton btn0=new JButton("0");
-		btn0.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn0);
-		
-		JButton btn14=new JButton(".");
-		btn14.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn14);
-		
-		JButton btn15=new JButton("=");
-		btn15.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn15);
-		
-		JButton btn16=new JButton("-");
-		btn16.setFont(new Font("",Font.BOLD,25));
-		f1.add(btn16);
-		
-		
+		for (int i = 0; i < 16; i++){
+			buttonArray[i]=new JButton(buttonName[i]);
+			buttonArray[i].setFont(new Font("",Font.BOLD,25));
+			f1.add(buttonArray[i]);
+		}*/
+
 		f1.setVisible(true);
 	}
 }
