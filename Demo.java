bbf84ca9bfa95c211eed3573c1542daa274be0fc
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-22 11:49:27 +0530

    lable

--- Demo.java
+++ Demo.java
@@ -3,33 +3,47 @@ import java.awt.*;
 class Demo{
 	public static void main(String[] args) {
 		JFrame f1=new JFrame();
-		f1.setSize(300,300);
+		f1.setSize(500,500);
 		f1.setTitle("Calculator");
 		f1.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		f1.setLayout(new GridLayout(4,4,2,2));
+		f1.setLayout(new FlowLayout());
+		f1.setLayout(new GridLayout(4,2,2,2));
 		
 		
-		JButton[] buttonArray = new JButton[16];
-		String[] buttonName = {"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
+		JLabel idL = new JLabel("ID");
+		idL.setFont(new Font("",1,25));
+		f1.add(idL);
 		
-		for(int i = 0;i<16;i++){
-			buttonArray[i] = new JButton(buttonName[i]);
-			buttonArray[i].setFont(new Font("",Font.BOLD,25));
-			f1.add(buttonArray[i]);
-		}
-	
-	
+		JTextField idT =new JTextField(5);
+		idT.setFont(new Font("",1,20));
+		f1.add(idT);
 		
-		/*JButton[] buttonArray=new JButton[16];
-		String[] buttonName={"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
+		JLabel nameL = new JLabel("Name");
+		nameL.setFont(new Font("",1,25));
+		f1.add(nameL);
 		
-		for (int i = 0; i < 16; i++){
-			buttonArray[i]=new JButton(buttonName[i]);
-			buttonArray[i].setFont(new Font("",Font.BOLD,25));
-			f1.add(buttonArray[i]);
-		}*/
-
+		JTextField nameT=new JTextField(5);
+		nameT.setFont(new Font("",1,20));
+		f1.add(nameT);
+		
+		JLabel prfL = new JLabel("PrfMarks");
+		prfL.setFont(new Font("",1,25));
+		f1.add(prfL);
+		
+		JTextField prfT=new JTextField(5);
+		prfT.setFont(new Font("",1,20));
+		f1.add(prfT);
+		
+		JLabel DbmsL = new JLabel("DBMS Marks");
+		DbmsL.setFont(new Font("",1,25));
+		f1.add(DbmsL);
+		
+		JTextField DbmsT=new JTextField(5);
+		DbmsT.setFont(new Font("",1,20));
+		f1.add(DbmsT);
+		
+		f1.pack();
 		f1.setVisible(true);
 	}
 }
