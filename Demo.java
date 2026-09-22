43a4146971c3e402ce52472a6a49f1b96ac4840d
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-22 13:54:20 +0530

    JFrame

--- Demo.java
+++ Demo.java
@@ -1,29 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+
+class Calculator extends JFrame{
+
+}
+
 class Demo{
 	public static void main(String[] args) {
-		JFrame f1=new JFrame();
-		f1.setSize(300,300);
-		f1.setTitle("Calculator");
-		f1.setLocationRelativeTo(null);
-		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
-		JTextField txtDisplay=new JTextField();
-		txtDisplay.setFont(new Font("",Font.BOLD,25));
-		txtDisplay.setHorizontalAlignment(JTextField.RIGHT);
-		//txtDisplay.setEditable(false);
-		f1.add("North",txtDisplay);
-		
-		
-		JButton[] buttonArray=new JButton[16];
-		String[] buttonName={"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
-		JPanel buttonPanel=new JPanel(new GridLayout(4,4,4,4));
-		for (int i = 0; i < 16; i++){
-			buttonArray[i]=new JButton(buttonName[i]);
-			buttonArray[i].setFont(new Font("",Font.BOLD,25));
-			buttonPanel.add(buttonArray[i]);
-		}
-		f1.add("Center",buttonPanel);
-		f1.setVisible(true);
+		Calculator c1=new Calculator();
+		c1.setSize(300,300);
+		c1.setTitle("Calculator");
+		c1.setLocationRelativeTo(null);
+		c1.setDefaultCloseOperation(Calculator.DISPOSE_ON_CLOSE);
+		c1.setVisible(true);
 	}
 }
