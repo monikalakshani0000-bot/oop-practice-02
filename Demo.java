8caf2484be9fa7440f15da83a1cc154f5b53f55a
Monika Lakshani <monikalakshani0000@gmail.com>
2026-09-22 13:35:07 +0530

    student details

--- Demo.java
+++ Demo.java
@@ -8,39 +8,40 @@ class Demo{
 		f1.setLocationRelativeTo(null);
 		f1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		JButton btnSouth=new JButton("South");
-		btnSouth.setFont(new Font("",Font.BOLD,25));
-		f1.add("South",btnSouth);
+		JLabel lblTitle=new JLabel("Add Student Form");
+		lblTitle.setFont(new Font("",1,28));
+		lblTitle.setHorizontalAlignment(JLabel.CENTER);
+		f1.add("North",lblTitle);
 		
-		JButton btnNorth=new JButton("North");
-		btnNorth.setFont(new Font("",Font.BOLD,25));
-		f1.add("North",btnNorth);
+		JTextField txtId=new JTextField(5);
+		txtId.setFont(new Font("",1,20));
+		JTextField txtName=new JTextField(10);
+		txtName.setFont(new Font("",1,20));
+		JTextField txtPrfMarks=new JTextField(3);
+		txtPrfMarks.setFont(new Font("",1,20));
+		JTextField txtDbmsMarks=new JTextField(3);
+		txtDbmsMarks.setFont(new Font("",1,20));
 		
-		JButton btnWest=new JButton("West");
-		btnWest.setFont(new Font("",Font.BOLD,25));
-		f1.add("West",btnWest);
+		JLabel lblId=new JLabel("Student ID");	
+		lblId.setFont(new Font("",1,20));
+		JLabel lblName=new JLabel("Name");	
+		lblName.setFont(new Font("",1,20));
+		JLabel lblPrfMarks=new JLabel("PRF Marks");	
+		lblPrfMarks.setFont(new Font("",1,20));
+		JLabel lblDbmsMarks=new JLabel("DBMS Marks");	
+		lblDbmsMarks.setFont(new Font("",1,20));
 		
-		JButton btnEast=new JButton("East");
-		btnEast.setFont(new Font("",Font.BOLD,25));
-		f1.add("East",btnEast);
-		
-		JButton btn1=new JButton("1");
-		btn1.setFont(new Font("",Font.BOLD,25));
-		JButton btn2=new JButton("2");
-		btn2.setFont(new Font("",Font.BOLD,25));
-		JButton btn3=new JButton("3");
-		btn3.setFont(new Font("",Font.BOLD,25));
-		JButton btn4=new JButton("4");
-		btn4.setFont(new Font("",Font.BOLD,25));
-		
-		JPanel centerPanel=new JPanel();
-		centerPanel.setLayout(new GridLayout(2,2));
-		centerPanel.add(btn1);
-		centerPanel.add(btn2);
-		centerPanel.add(btn3);
-		centerPanel.add(btn4);
-		
-		f1.add("Center",centerPanel);
+		JPanel panel=new JPanel(new GridLayout(4,2));	
+		panel.add(lblId);
+		panel.add(txtId);
+		panel.add(lblName);	
+		panel.add(txtName);	
+		panel.add(lblPrfMarks);	
+		panel.add(txtPrfMarks);	
+		panel.add(lblDbmsMarks);
+		panel.add(txtDbmsMarks);
+		f1.add("West",panel);
+		f1.pack();
 		f1.setVisible(true);
 	}
 }
